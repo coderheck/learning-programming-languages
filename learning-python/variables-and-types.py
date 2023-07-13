50a39bba40f62d206613e0151e2525ad0d511a8f
coderheck <loxzyioxzybusiness@hotmail.com>
2023-07-13 22:02:53 +0700

    strings, nums, concats

--- learning-python/variables-and-types.py
+++ learning-python/variables-and-types.py
@@ -2,3 +2,17 @@ integer = 420
 floatNum1 = 69.0
 floatNum2 = float(69)
 print(integer, floatNum1, floatNum2)
+
+testString1 = "rayhanning"
+testString2 = 'the'
+print(testString2, testString1)
+
+num1, num2 = 1, 2
+print(num1 + num2 * num2)
+
+stringToConcat1 = "string"
+stringToConcat2 = "concatenation"
+stringToConcat3 = "is like this"
+stringAfterConcat = stringToConcat1 + " " + stringToConcat2 + " " + stringToConcat3
+print(stringAfterConcat)
+print(stringToConcat1 + " " + stringToConcat2 + " " + stringToConcat3)
