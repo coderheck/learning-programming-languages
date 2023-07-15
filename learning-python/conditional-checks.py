f81a739ca75cb2eb38dcb7abbca55c265111243d
coderheck <loxzyioxzybusiness@hotmail.com>
2023-07-15 15:40:32 +0700

    why is "true" and "false" in python capitalized

---
+++ learning-python/conditional-checks.py
@@ -0,0 +1,9 @@
+true = True 
+false = False
+# why is "true" and "false" in python capitalized
+if true == False:
+    print("true is not True")
+elif false == True:
+    print("false is not False")
+else:
+    print("i hate my life")
