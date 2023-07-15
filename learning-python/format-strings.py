345f146a885eedc7236adb0c68ea6ba59e8e0f80
coderheck <loxzyioxzybusiness@hotmail.com>
2023-07-15 14:43:58 +0700

    format strings

--- learning-python/format-strings.py
+++ learning-python/format-strings.py
@@ -1,2 +1,14 @@
 str1 = "hello"
 str2 = "python"
+float1 = 8.21234352342781926
+pi = 3.14159
+
+print("from loxzy: %s %s!" % (str1, str2))
+print("the first 6 digits of pi: %f" % pi)
+print("the first 7 digits of \"float1\": %.6f" % float1)
+
+lotsOfData = ("guy named loxzy", 90812, 1.28767289)
+stringToPrint = "data: %s, %d, %f"
+print(stringToPrint % lotsOfData)
+
+# end
