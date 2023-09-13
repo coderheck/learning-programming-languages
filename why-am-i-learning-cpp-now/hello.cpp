2065be5520bfb663290476baea427dc8130f2375
coderheck <loxzyioxzybusiness@hotmail.com>
2023-09-13 22:04:21 +0700

    final update for today

--- why-am-i-learning-cpp-now/hello.cpp
+++ why-am-i-learning-cpp-now/hello.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 int main() {
-    cout << "hello world";
+    cout << "hello world! ";
+    cout << "this is loxzy speaking, ";
+    cout << "this should work or im killing myself";
     return 0;
 }
