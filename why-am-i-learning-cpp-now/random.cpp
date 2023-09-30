d2b6cc7012e089350aaf8e754cae079df2a86a37
coderheck <loxzyioxzybusiness@hotmail.com>
2023-09-30 18:07:02 +0700

    yank

--- why-am-i-learning-cpp-now/random.cpp
+++ why-am-i-learning-cpp-now/random.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <cmath>
+#define max 1000000000000000000
 using namespace std;
 
 int main() {
-    long long a, b, c;
-    cin >> a >> b >> c;
-    cout << a + (b*c);
+    int a, a1;
+    cin >> a;
+    if (0 < a <= max) {
+        a1 = sqrt(a);
+        cout << a + a1;
+    }else{return 0;}
     return 0;
 }
