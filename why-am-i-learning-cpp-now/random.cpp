0ced2056bb7db1374115bc5e444a29887341a834
coderheck <loxzyioxzybusiness@hotmail.com>
2023-10-05 15:00:01 +0700

    end

--- why-am-i-learning-cpp-now/random.cpp
+++ why-am-i-learning-cpp-now/random.cpp
@@ -2,13 +2,7 @@
 using namespace std;
 
 int main(){
-    long long n, tongChan, tongLe;
-    tongChan = tongLe = 0;
-    cout << "nhap n";
-    cin >> n;
-    for (int i = 1; i = n; i++){
-        if (i % 2 == 0){tongChan + i;}else{tongLe + i;}
-    }
-    cout << "tong cac so chan tu 1 den n: " << tongChan << endl << "tong cac so le tu 1 den n: " << tongLe << endl;
     return 0;
 }
+
+// default snippet for all later code
