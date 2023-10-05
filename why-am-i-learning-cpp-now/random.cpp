d1d80aa7017a7456f172543859a249e80ef7de7d
coderheck <loxzyioxzybusiness@hotmail.com>
2023-10-05 13:53:01 +0700

    dies

--- why-am-i-learning-cpp-now/random.cpp
+++ why-am-i-learning-cpp-now/random.cpp
@@ -2,8 +2,16 @@
 using namespace std;
 
 int main() {
-    long long a, b;
+    long long a, b, num;
+    num = 0;
     cin >> a >> b;
-    if (a*a > b*b){cout<<a;}else{cout<<b;}
+    if (1 < a < b <= 100) {
+        for (int count = 1; count <= b; count++){
+        if (count % 3 == 0) {
+            num += 1;
+        }
+    }
+    cout << num;
+    }else{return 0;}
     return 0;
 }
