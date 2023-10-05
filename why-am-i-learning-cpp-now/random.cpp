d2a1d402ecc60610dbebaead011b7d08a568fc6a
coderheck <loxzyioxzybusiness@hotmail.com>
2023-10-05 14:55:12 +0700

    i cant

--- why-am-i-learning-cpp-now/random.cpp
+++ why-am-i-learning-cpp-now/random.cpp
@@ -1,25 +1,14 @@
-#include <iostream>
-#include <math.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int Ox, Oy, R, Ax, Ay;
-    float OA;
-    cout << "nhap toa do tam O va ban kinh R (thu tu: Ox -> Oy -> ban kinh R)" << endl;
-    cin >> Ox >> Oy >> R;
-    cout << "nhap toa do diem A (thu tu: Ax -> Ay)" << endl;
-    cin >> Ax >> Ay;
-    cout << "do dai OB, AB la:" << endl;
-    cout << "OB: " << Ax - Ox << ", AB: " << Ay - Oy << endl;
-    OA = sqrt((Ax-Ox)*(Ax-Ox)+(Ay-Oy)*(Ay-Oy));
-    cout << "do dai OA la: " << OA << endl;
-    if (OA > R){
-        cout << "diem A nam ngoai duong tron";
-    }else if (OA < R){
-        cout << "diem A nam trong duong tron";
-    }else if (OA = R){
-        cout << "diem A nam tren duong tron";
-    }else{cout << "dude wtf";}
+int main(){
+    long long n, tongChan, tongLe;
+    tongChan = tongLe = 0;
+    cout << "nhap n";
+    cin >> n;
+    for (int i = 1; i = n; i++){
+        if (i % 2 == 0){tongChan + i;}else{tongLe + i;}
+    }
+    cout << "tong cac so chan tu 1 den n: " << tongChan << endl << "tong cac so le tu 1 den n: " << tongLe << endl;
     return 0;
 }
-//lol okay i used the wrong formula
