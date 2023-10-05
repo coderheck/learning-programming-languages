8b7795758d96904417894a26b8480b2d341a625e
coderheck <loxzyioxzybusiness@hotmail.com>
2023-10-05 16:35:59 +0700

    die

--- why-am-i-learning-cpp-now/SumOfEvenNumbersAndOddNumberToN.cpp
+++ why-am-i-learning-cpp-now/SumOfEvenNumbersAndOddNumberToN.cpp
@@ -2,13 +2,45 @@
 using namespace std;
 
 int main(){
-    long long n, tongChan, tongLe;
-    tongChan = tongLe = 0;
+    long long n, tong1, tong2;
+    tong1 = tong2 = 0;
     cout << "nhap n: ";
     cin >> n;
+
+    cout << "cac so nguyen tu 1 den n: ";
     for (int i = 1; i <= n; i++){
-        if (i % 2 == 0){tongChan += i;}else{tongLe += i;}
+        cout << i << " ";
     }
-    cout << "tong cac so chan tu 1 den n: " << tongChan << endl << "tong cac so le tu 1 den n: " << tongLe << endl;
+
+    cout << endl << "cac so chan tu 1 den n: ";
+    for (int i = 1; i <= n; i++){
+        if (i % 2 == 0){cout << i << " ";}
+    }
+
+    cout << endl << "cac so chia het cho 3 tu 1 den n: ";
+    for (int i = 1; i <= n; i++){
+        if (i % 3 == 0){cout << i << " ";}
+    }
+
+    cout << endl << "cac so chia het cho 3 tu 1 den n la: ";
+    for (int i = 1; i <= n; i++){
+        if (i % 3 == 0){
+            cout << i << " ";
+            tong1 += i;
+        }
+    }
+    cout << "; tong cac so tren la: " << tong1;
+
+    cout << endl << "cac so le chia het cho 3 tu 1 den n la: ";
+    for (int i = 1; i <= n; i++){
+        if (i % 3 == 0 and i % 2 != 0){
+            cout << i << " ";
+            tong2 += i;
+        }
+    }
+
     return 0;
 }
+/*
+
+*/
