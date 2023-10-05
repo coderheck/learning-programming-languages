89449ba4e882ba72b7ebd074274277ad852460c1
coderheck <loxzyioxzybusiness@hotmail.com>
2023-10-05 14:20:45 +0700

    commit

--- why-am-i-learning-cpp-now/random.cpp
+++ why-am-i-learning-cpp-now/random.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 int main() {
-    long long a, b, num;
-    num = 0;
-    cin >> a >> b;
-    if (1 < a < b <= 100) {
-        for (int count = 1; count <= b; count++){
-        if (count % 3 == 0) {
-            num += 1;
-        }
-    }
-    cout << num;
-    }else{return 0;}
+    float Ox, Oy, R, Ax, Ay, OA;
+    cout << "nhap toa do tam O va ban kinh R (thu tu: Ox -> Oy -> ban kinh R)" << endl;
+    cin >> Ox >> Oy >> R;
+    cout << "nhap toa do diem A (thu tu: Ax -> Ay)" << endl;
+    cin >> Ax >> Ay;
+    cout << "do dai OB, AB la:" << endl;
+    cout << "OB: " << Ax - Ox << ", AB: " << Ax - Oy << endl;
+    OA = sqrt(Ax - Ox) * (Ax - Ox) + (Ay - Oy) * (Ay - Oy);
+    cout << "do dai OA la:" << endl << OA << endl;
+    if (OA > R){
+        cout << "diem A nam ngoai duong tron";
+    }else if (OA < R){
+        cout << "diem A nam trong duong tron";
+    }else if (OA = R){
+        cout << "diem A nam tren duong tron";
+    }else{cout << "dude wtf";}
     return 0;
 }
