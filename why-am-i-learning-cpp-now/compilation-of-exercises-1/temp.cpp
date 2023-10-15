4325a09f904965e5d12c34d104e454ac87a163d6
coderheck <loxzyioxzybusiness@hotmail.com>
2023-10-15 17:14:08 +0700

    nic

--- why-am-i-learning-cpp-now/compilation-of-exercises-1/temp.cpp
+++ why-am-i-learning-cpp-now/compilation-of-exercises-1/temp.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 int main(){
-    long long n, iter, two, five;
+    long long n, iter, two, five, n2, n5;
     two = five = 0;
     cout << "enter amount of usd to pay:" << endl;
     cin >> n;
-    if (n>=2 and n>=5){
-        for (iter=n; iter<=0; iter-2){
-            if (iter-2>=0){
-                two+=1;
-            }
-        }
+    for (iter = n; iter >= 1; iter -= 2){
+        two += 1;
     }
-    cout << two << " steps for using just 2 usd" << endl;
-    cout << five << " steps for using just 5 usd" << endl;
+    for (iter = n; iter >= 4; iter -= 5){
+        five += 1;
+    }
+    cout << two << " time(s) 2 usd" << endl;
+    cout << "or" << endl;
+    cout << five << " time(s) 5 usd" << endl;
     return 0;
 }
 
