f415415eeb26cf22db75b9b1d581b09003d4e4d7
coderheck <loxzyioxzybusiness@hotmail.com>
2023-11-16 16:23:46 +0700

    comes

--- why-am-i-learning-cpp-now/peterdicho.cpp
+++ why-am-i-learning-cpp-now/peterdicho.cpp
@@ -9,7 +9,7 @@ int main(){
     cin >> n;
     for (int n1=0; n1<=n; n1++){
         for (int n2=0; n2<=n; n2++){
-            if ((n1*2) + (n2*5) == n){cout<<n1<<"to 2 usd va "<<n2<<" to 5 usd"<<endl;}
+            if ((n1*2) + (n2*5) == n){cout<<n1<<" to 2 usd va "<<n2<<" to 5 usd"<<endl;}
         }
     }
     return 0;     
