8675a9a9c52e5cdecf42e261602329df6b480921
coderheck <loxzyioxzybusiness@hotmail.com>
2023-11-25 20:45:38 +0700

    a

---
+++ why-am-i-learning-cpp-now/nov 23rd 2023/6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    long long a,b,c;
+    cin>>a>>b;
+    for (long long a1=0;a1>=1;a1++){
+        for (long long b1=0;b1>=1;b1++){
+            if ((a*a1)==(b*b1)){cout<<(a*a1);}
+        }
+    }
+    cout<<c;
+    return 0;
+}
