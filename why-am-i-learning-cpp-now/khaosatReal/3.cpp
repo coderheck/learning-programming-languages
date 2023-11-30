131dc96f2a9c92696623f21541f641f848bab5cd
coderheck <loxzyioxzybusiness@hotmail.com>
2023-11-30 15:35:13 +0700

    fix

--- why-am-i-learning-cpp-now/khaosatReal/3.cpp
+++ why-am-i-learning-cpp-now/khaosatReal/3.cpp
@@ -1,26 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long tien,nam,mot,hai,haichuc,namchuc;
+long long tien,nam,mot,hai,haimuoi,nammuoi;
 int main(){
     cin >> tien;
     if (tien / 50 >= 1){
-        namchuc = tien/50;
-        cout << namchuc << " to 50" << endl;
+        nammuoi = tien/50;
+        cout << nammuoi << " to 50" << endl;
     }
-    if ((tien - namchuc) / 20 >= 1){
-        haichuc = (tien - (namchuc*50)) / 20;
-        cout << haichuc << " to 20";
+    if ((tien - nammuoi) / 20 >= 1){
+        haimuoi = (tien - (nammuoi*50)) / 20;
+        cout << haimuoi << " to 20";
     }
-    if ((tien - (namchuc*50) - (haichuc*20)) / 5 >= 1){
-        nam = (tien - (namchuc*50) - (haichuc*20)) / 5;
+    if ((tien - (nammuoi*50) - (haimuoi*20)) / 5 >= 1){
+        nam = (tien - (nammuoi*50) - (haimuoi*20)) / 5;
         cout << nam << " to 5" << endl;
     }
-    if ((tien - (namchuc*50) - (haichuc*20) - (nam*5)) / 2 >= 1){
-        hai = (tien - (namchuc*50) - (haichuc*20) - (nam*5)) / 2;
+    if ((tien - (nammuoi*50) - (haimuoi*20) - (nam*5)) / 2 >= 1){
+        hai = (tien - (nammuoi*50) - (haimuoi*20) - (nam*5)) / 2;
         cout << hai << "to 2" << endl;
     }
-    if ((tien - (namchuc*50) - (haichuc*20) - (nam*5) - (hai*2)) / 1 >= 1){
-        mot = (tien - (namchuc*50) - (haichuc*20) - (nam*5) - (hai*2)) / 1;
+    if ((tien - (nammuoi*50) - (haimuoi*20) - (nam*5) - (hai*2)) / 1 >= 1){
+        mot = (tien - (nammuoi*50) - (haimuoi*20) - (nam*5) - (hai*2)) / 1;
         cout << mot << " to 1";
     }
 }
