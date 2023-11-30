dbc667f8cc6bf72594c8b2ae7b03131c13bcccc3
coderheck <loxzyioxzybusiness@hotmail.com>
2023-11-30 15:33:02 +0700

    done WOOOOOO

--- why-am-i-learning-cpp-now/khaosatReal/5.cpp
+++ why-am-i-learning-cpp-now/khaosatReal/5.cpp
@@ -1,12 +1,12 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-bool isPrime(long long n){
-    if (n <= 1){return false;}else{
-        for (int i=2;i<=n/2;i++){
-            if (n % i == 0){return false;}else{return true;}
-        }
-    }
+bool isPrime(int n){
+    if (n <= 1)
+        return false;
+    for (int i=2;i<=n/2;i++)
+        if (n % i == 0){return false;}
+    return true;
 }
 long long daochuso(long long arg){
     long long n=arg, tmp, retNum=0;
@@ -15,13 +15,14 @@ long long daochuso(long long arg){
         retNum = retNum * 10 + tmp;
         n = n / 10;
     }
-    cout << retNum;
+    return retNum;
 }
-
 int main(){
-    long long a, b;
+    long long a, b, so=0;
+    cin>>a>>b;
     for (long long n=min(a, b); n<=max(a,b); n++){
-        if(isPrime(n)==true){}
+        if(isPrime(n)==true && isPrime(daochuso(n))==true){so+=1;}
     }
+    cout<<so;
     return 0;
 }
