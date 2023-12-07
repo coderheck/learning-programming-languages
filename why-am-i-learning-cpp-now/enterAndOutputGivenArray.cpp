05213b136da020052c2ff72f0a01f6d58882dfb1
coderheck <loxzyioxzybusiness@hotmail.com>
2023-12-07 15:36:56 +0700

    changes

--- why-am-i-learning-cpp-now/enterAndOutputGivenArray.cpp
+++ why-am-i-learning-cpp-now/enterAndOutputGivenArray.cpp
@@ -1,15 +1,17 @@
-#include <iostream>
+#include "iostream"
 using namespace std;
 
 int main(){
-    int a;
+    int a,sum=0;
     cin>>a;
     int arr[a];
     for(int i1=0; i1<a; i1++){
+        cout<<"arr["<<i1<<"] = ";
         cin>>arr[i1];
     }
     for (int i2=0; i2<a; i2++){
-        cout<<arr[i2]<<" ";
+        cout<<arr[i2]<<" "<<endl;sum+=arr[i2];
     }
+    cout<<"sum = "<<sum;
     return 0;
 }
