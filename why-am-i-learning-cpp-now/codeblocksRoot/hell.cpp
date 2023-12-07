7636443b8585828180cc989a2b9507ef150d77f8
coderheck <loxzyioxzybusiness@hotmail.com>
2023-12-07 14:19:05 +0700

    edits

--- why-am-i-learning-cpp-now/codeblocksRoot/hell.cpp
+++ why-am-i-learning-cpp-now/codeblocksRoot/hell.cpp
@@ -3,8 +3,14 @@
 using namespace std;
 
 int main(){
-    string h;
-    cin>>h;
-    cout<<h;
+    int n;
+    cin>>n;
+    // do while structure
+    // syntax:
+    do {
+        //whatever
+        if (n<0){cout<<"nhap lai sao cho n > 0"<<endl;cin>>n;}
+    }
+    while (/*condition*/n<0);
     return 0;
 }
