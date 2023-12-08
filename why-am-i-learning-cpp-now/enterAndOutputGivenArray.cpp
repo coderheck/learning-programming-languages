b98f6f729be5598772fea7c987c8fac6d8d721ff
coderheck <loxzyioxzybusiness@hotmail.com>
2023-12-08 17:18:46 +0700

    the missing parts

--- why-am-i-learning-cpp-now/enterAndOutputGivenArray.cpp
+++ why-am-i-learning-cpp-now/enterAndOutputGivenArray.cpp
@@ -11,16 +11,34 @@ int main(){
     for (int i=0; i<a; i++){
         cout<<b[i]<<" "<<endl;sum+=b[i];
     }
-    cout<<endl<<"tang dan:"<<endl;
+
+    cout<<endl<<"so chan:"<<endl;
     for (int i=0; i<a; i++){
-        //if (b[i]>b[i+1]){swap(b[i], b[i+1]);}
+        if (b[i]%2==0){cout<<b[i]<<" "<<endl;sum+=b[i];}
+    }
+
+    cout<<endl<<"so le:"<<endl;
+    for (int i=0; i<a; i++){
+        if (b[i]%2!=0){cout<<b[i]<<" "<<endl;sum+=b[i];}
+    }
+
+    cout<<endl<<"so chia het cho 2 va 5:"<<endl;
+    for (int i=0; i<a; i++){
+        if (b[i]%2==0&&b[i]%5==0){cout<<b[i]<<" "<<endl;sum+=b[i];}
+    }
+
+    // sap xep bien
+    for (int i=0; i<a; i++){
         for (int o=i; o<a; o++){
             if (b[i]>b[o]){swap(b[i], b[o]);}
         }
     }
+
+    cout<<endl<<"tang dan:"<<endl;
     for (int i=0; i<a; i++){
         cout<<b[i]<<" ";
     }
+
     cout<<endl<<"giam dan:"<<endl;
     for (int i=a-1; i>=0; i--){
         cout<<b[i]<<" ";
