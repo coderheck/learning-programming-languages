2e1f6eaa29196cfa6054c18f0881762cf9596e25
coderheck <loxzyioxzybusiness@hotmail.com>
2023-12-14 14:49:20 +0700

    ez

--- why-am-i-learning-cpp-now/test.cpp
+++ why-am-i-learning-cpp-now/test.cpp
@@ -1,15 +1,22 @@
 #include"iostream"
 #include"cmath"
+bool isSquare(long long a){
+    if (floor(sqrt(a))==sqrt(a) and ceil(sqrt(a))==sqrt(a)){return true;}else{return false;}
+}
+void hello1(int so1, int so2){
+    std::cout<<"xinchao";
+    if (isSquare(so1)==true){std::cout<<std::endl<<"nice";}
+    std::cout<<std::endl<<so1+so2;
+    return;
+}
+long long hello2(int a){
+    return a*a;
+}
 
-bool chinhphuong(long long a){
-    bool ch=true;
-    float sq=std::sqrt(a);
-    if(std::ceil(sq) != std::floor(sq)){ch=false;}
-    return ch;
-}
 int main(){
-    long long n;
-    std::cin>>n;
-    std::cout<<chinhphuong(n);
-    return 0;
+    int a, b, c;
+    std::cin>>a>>b>>c;
+    hello1(a,b);
+    std::cout<<std::endl<<hello2(c);
 }
+
