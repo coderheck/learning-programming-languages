50b2b5dc643bdffa86074773dbc46d4189dde852
coderheck <loxzyioxzybusiness@hotmail.com>
2023-12-14 15:21:35 +0700

    commite

--- why-am-i-learning-cpp-now/test.cpp
+++ why-am-i-learning-cpp-now/test.cpp
@@ -3,6 +3,14 @@
 bool isSquare(long long a){
     if (floor(sqrt(a))==sqrt(a) and ceil(sqrt(a))==sqrt(a)){return true;}else{return false;}
 }
+bool isPrime(long long n){
+    bool isprime=true;
+    if(n<=1){isprime=false;}
+    for(long long i=2;i<=n/2;i++){
+        if(n%i==0){isprime=false;}
+    }
+    return isprime;
+}
 void hello1(int so1, int so2){
     std::cout<<"xinchao";
     if (isSquare(so1)==true){std::cout<<std::endl<<"nice";}
@@ -14,9 +22,10 @@ long long hello2(int a){
 }
 
 int main(){
-    int a, b, c;
+    int a, b, c, d;
     std::cin>>a>>b>>c;
     hello1(a,b);
     std::cout<<std::endl<<hello2(c);
+    std::cout<<std::endl<<isPrime(d);
 }
 
