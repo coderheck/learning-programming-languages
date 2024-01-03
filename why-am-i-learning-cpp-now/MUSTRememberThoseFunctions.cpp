ea3720b993e46e98c9c586f2f8c63c3f82a1dd28
coderheck <loxzyioxzybusiness@hotmail.com>
2024-01-03 17:29:29 +0700

    made the code looks worse

--- why-am-i-learning-cpp-now/MUSTRememberThoseFunctions.cpp
+++ why-am-i-learning-cpp-now/MUSTRememberThoseFunctions.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cmath>
-using namespace std;
 
 bool isPrime(long long n){
     bool isprime=true;
@@ -12,30 +11,22 @@ bool isPrime(long long n){
 }
 long long digitsSum(long long arg){
     int n=arg,sum=0;
-    while(n != 0) {
-        sum += n%10;
-        n /= 10;
-    }
+    while(n!=0){sum+=n%10;n/=10;}
     return sum;
 }
 bool isSquare(long long a){
-    if (floor(sqrt(a))==sqrt(a) and ceil(sqrt(a))==sqrt(a)){return true;}else{return false;}
+    bool s;
+    if(floor(sqrt(a))==sqrt(a)&&ceil(sqrt(a))==sqrt(a)){s=true;}else{s=false;}
+    return s;
 }
 long long reverseDigits(long long arg){
-    long long n=arg, tmp, retNum=0;
-    while(n > 0){
-        tmp = n % 10;
-        retNum = retNum * 10 + tmp;
-        n = n / 10;
-    }
-    return retNum;
+    long long n=arg,tmp,returnNumber=0;
+    while(n>0){tmp=n%10;returnNumber=returnNumber*10+tmp;n/=10;}
+    return returnNumber;
 }
 bool isPerfect(long long arg){
-    long long sum=0;
-    bool isperfect;
-    for (long long i=1;i<arg;i++){
-        if (arg%i==0){sum+=i;}
-    }
+    long long sum=0;bool isperfect;
+    for (long long i=1;i<arg;i++){if(arg%i==0){sum+=i;}}
     if(arg==sum){isperfect=true;}else{isperfect=false;}
     return isperfect;
 }
