6eec3e7ecdad1835aa59ade1f6b95487525e3edc
coderheck <loxzyioxzybusiness@hotmail.com>
2024-01-05 21:58:58 +0700

    half working

--- why-am-i-learning-cpp-now/home/loops-2.cpp
+++ why-am-i-learning-cpp-now/home/loops-2.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main(){
     int demtu=1,demkitu=0;
     char c='c';
-    while(c!='\r'){
+    while(c!='\n'){
         c=getchar();
         if(c==' '){demtu++;}else{demkitu++;}
     }
-    cout<<c<<"\n"<<"so tu: "<<"\n"<<"; so ki tu: "<<demkitu-1;
+    cout<<"so tu: "<<"\n"<<"so ki tu: "<<demkitu;
 }
