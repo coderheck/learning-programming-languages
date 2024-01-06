0bfc924cafc4e90503559d7e25ff8b4cf285cdf1
coderheck <loxzyioxzybusiness@hotmail.com>
2024-01-06 08:16:11 +0700

    added one function to memorable functions

--- why-am-i-learning-cpp-now/MUSTRememberThoseFunctions.cpp
+++ why-am-i-learning-cpp-now/MUSTRememberThoseFunctions.cpp
@@ -30,6 +30,10 @@ bool isPerfect(long long arg){
     if(arg==sum){isperfect=true;}else{isperfect=false;}
     return isperfect;
 }
+int gcd(int a, int b){
+    while(a!=b){if(a>b){a-=b;}else{b-=a;}}
+    return a;
+}
 int main(){
     int x; std::cin>>x;
     std::cout<<reverseDigits(x);
