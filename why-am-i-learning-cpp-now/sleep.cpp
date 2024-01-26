310e39d6c97c1a951d5d251045bf90ed85539d47
coderheck <loxzyioxzybusiness@hotmail.com>
2024-01-27 00:26:33 +0700

    go to sleep

--- why-am-i-learning-cpp-now/sleep.cpp
+++ why-am-i-learning-cpp-now/sleep.cpp
@@ -2,11 +2,12 @@
 #include"iostream"
 using namespace std;
 int main(){
-    cout<<"hold on for 3 seconds"<<"\n";
+    cout<<"hold on for 3 seconds..."<<"\n";
+    cout<<"3"<<"\n";
     Sleep(1000);
     cout<<"2"<<"\n";
     Sleep(1000);
     cout<<"1"<<"\n";
     Sleep(1000);
-    cout<<"boom!!!"<<"\n";
+    cout<<"boom!!!";
 }
