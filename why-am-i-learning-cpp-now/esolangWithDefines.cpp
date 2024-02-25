396c021304ea049cdc46ec9a4cb81a6dab669b8a
coderheck <loxzyioxzybusiness@hotmail.com>
2024-02-26 00:10:10 +0700

    got the bussin vocab 🔥

---
+++ why-am-i-learning-cpp-now/esolangWithDefines.cpp
@@ -0,0 +1,49 @@
+#define chief main
+#define nocap true
+#define cap false
+#define lawg bool
+#define yeet return
+#define sussin if
+#define shit else
+#define bussin for
+#define tweakin while
+#define gotta std
+#define felloff endl
+#define spit cout
+#define slurp cin
+#define fr <<
+#define deadass >>
+#define rn ;
+#define mf int
+#define nah 0
+#define be =
+#define finna ==
+#define smallASF <=
+#define fuck %
+#define cut /
+#define nope !
+#define stepup ++
+#define bitches "iostream"
+#include bitches
+
+lawg prime(mf n){
+    lawg p be nocap rn
+    sussin(n smallASF 1){p be cap rn}
+    bussin(mf i be 2 rn i smallASF n cut 2 rn i stepup){
+        sussin(n fuck i finna 0){p be cap rn}
+    }
+    yeet p rn
+}
+
+mf chief(){
+    mf n be 0 rn
+    tweakin (nocap){
+        gotta::slurp deadass n rn 
+        sussin(prime(n) finna true){
+            gotta::spit fr "is a prime" fr gotta::felloff rn
+        }
+        shit{
+            gotta::spit fr "is not a prime" fr gotta::felloff rn
+        }
+    }
+}
