17a58a9ed63257b16a0fc794980eb883fce808d1
coderheck <loxzyioxzybusiness@hotmail.com>
2024-03-01 00:37:26 +0700

    ok

--- why-am-i-learning-cpp-now/test.cpp
+++ why-am-i-learning-cpp-now/test.cpp
@@ -1,14 +1,9 @@
-#include"iostream"
 using namespace std;
-bool nguyento(int n){
-    bool nt=true;
-    if(n<=1){nt=false;}
-    for(int i=2;i<=n/2;i++){if(n%i==0){nt=false;}}
-    return nt;
-}
+#include"iostream"
 int main(){
-    int n;
-    while(true){
-        cin>>n;if(nguyento(n)==true){cout<<"nguyen to"<<"\n";}else if(nguyento(n)==false){cout<<"so phuc"<<"\n";}else if(nguyento(n)==0){cout<<"so 0"<<"\n";}else if(nguyento(n)==1){cout<<"so 1"<<"\n";}
+    int n[100],c=0;
+    for(int i=10;i<100;i++){
+        if(i%3==1 && i%5==1){n[c]=i;c++;}
     }
+    for(int i=0;i<c;i++){cout<<n[i]<<" ";}
 }
