0ab22e6194bdcae30b9cb46a5ecda21f15b81d63
coderheck <loxzyioxzybusiness@hotmail.com>
2024-03-02 22:44:07 +0700

    s

--- why-am-i-learning-cpp-now/interestingFunctions.cpp
+++ why-am-i-learning-cpp-now/interestingFunctions.cpp
@@ -45,6 +45,43 @@ void countSort(int array[], int size) {
     array[i]=output[i];
   }
 }
+void eratosthenes_sieve(bool arr[],int n){
+  // init prime table
+  for (int i=0;i<=n;i++){arr[i]=true;}
+  // 0 and 1 arent primes
+  arr[0]=arr[1]=false;
+  // if i is not a prime then all its multiples are not primes
+  for (int i=2;i*i<=n;i++){
+    if (arr[i]){
+      for (int j=i*i;j<=n;j+=i){arr[j]=false;}
+    }
+  }
+}
+#include "vector"
+std::vector<int> dijkstraPrimeAlgorithm(int numberOfPrimes){ // this requires the vector library, also this isnt complete
+  std::vector <int> primes = {2}, Q; int x=1, limit=4;
+  auto is_prime = [&](int x){
+    bool p=true;
+    for(int k=2; k<Q.size(); k++){
+      if(x>Q[k])
+        Q[k]+=primes[k];
+      if(x==Q[k])
+        p=false;
+    }
+    return p;
+  };
+  while(primes.size()<numberOfPrimes){
+    do{
+      x+=2;
+      if(x>=limit){
+        Q[Q.size()], limit = limit, primes[Q.size()+1]*primes[Q.size()+1];
+      }
+    }while(!is_prime(x));
+    primes.push_back(x);
+  }
+  return primes;
+}
+
 int main(){
 
 }
