ab7b828a67df0c71994f6591bb75351dfcb42cf1
coderheck <loxzyioxzybusiness@hotmail.com>
2024-03-07 20:29:38 +0700

    idk again

--- why-am-i-learning-cpp-now/mathhomework.cpp
+++ why-am-i-learning-cpp-now/mathhomework.cpp
@@ -9,7 +9,7 @@ bool isSquare(long long a){
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    for(int i=1;i<15;i++){
+    for(int i=1;i<200;i++){
         if(i%7==0){cout<<i<<" ";}
     }
 }
