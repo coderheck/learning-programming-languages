4ebc871ea0fed20e9af41792475157c327679e47
coderheck <loxzyioxzybusiness@hotmail.com>
2024-03-27 09:33:26 +0700

    nice

--- why-am-i-learning-cpp-now/codeblocksRoot/temp.cpp
+++ why-am-i-learning-cpp-now/codeblocksRoot/temp.cpp
@@ -1,14 +1,6 @@
-#include"bits/stdc++.h"
+#include"iostream"
 using namespace std;
 int main(){
-    int n,s,c=0;cin>>n>>s;int chieurong[n];
-    for(int i=0;i<n;i++){cin>>chieurong[i];}
-    for(int i=0;i<n;i++){cout<<chieurong[i]<<" ";}
-    cout<<"\n";
-    for(int i=0;i<n;i++){
-        for(int j=i;j<n;j++){
-            if(i+j<s){c++;}
-        }
-    }
-    cout<<c;
+  long long a,b;cin>>a>>b;
+  if(a<b){cout<<b;}else{cout<<a;}
 }
