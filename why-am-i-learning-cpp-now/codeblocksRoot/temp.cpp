46a74f721b17aa4e491bf0a19cc0a7b37ee4041c
coderheck <loxzyioxzybusiness@hotmail.com>
2024-03-29 14:31:07 +0700

    code

--- why-am-i-learning-cpp-now/codeblocksRoot/temp.cpp
+++ why-am-i-learning-cpp-now/codeblocksRoot/temp.cpp
@@ -1,6 +1,14 @@
 #include"iostream"
+#define ll long long
+ll gcd(ll a, ll b){
+  ll c=a,d=b;
+  while(c!=b){if(c>b){c-=b;}else{b-=c;}}
+  return c;
+}
 using namespace std;
 int main(){
-  long long a,b;cin>>a>>b;
-  if(a<b){cout<<b;}else{cout<<a;}
+  ll a,b;cin>>a>>b;
+  if(b==0){cout<<"ERROR";}else{
+    cout<<a/gcd(a,b)<<" "<<b/gcd(a,b);
+  }
 }
