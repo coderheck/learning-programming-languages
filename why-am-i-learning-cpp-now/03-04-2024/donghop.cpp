a152f7c4b465b4232c6072d3a77ee8cacaa09de1
coderheck <loxzyioxzybusiness@hotmail.com>
2024-04-03 10:08:33 +0700

    h

--- why-am-i-learning-cpp-now/03-04-2024/donghop.cpp
+++ why-am-i-learning-cpp-now/03-04-2024/donghop.cpp
@@ -1,4 +1,5 @@
 #include"bits/stdc++.h"
+using namespace std;
 #define ll long long
 #define kien main
 #define kbc ios_base::sync_with_stdio(false);cin.tie(nullptr);
@@ -9,7 +10,10 @@ int kien(){
     freopen("donghop.inp","r",stdin);
     freopen("donghop.out","w",stdout);
   }
-  int n,k;ll t=0;cin>>n>>k;
-  for(int i=1;i<=n;i++){int a;cin>>a;t+=a;}
+  int n,k;ll t=0,s=0;cin>>n>>k;ll a[n];
+  for(int i=1;i<=n;i++){cin>>a[i];}
+  for(int i=1;i<=n;i++){
+    if(a[i]==k){s++;}else if(a[i]>k){}else if(a[i]<k){t+=a[i];}
+  }
   cout<<t/k*1.0;
 }
