ff50414381224747a9613bab69c1804e14ff4d6a
coderheck <loxzyioxzybusiness@hotmail.com>
2024-04-04 16:11:12 +0700

    what

--- why-am-i-learning-cpp-now/tempCodeRunnerFile.cpp
+++ why-am-i-learning-cpp-now/tempCodeRunnerFile.cpp
@@ -1,9 +1,14 @@
-#include"bits/stdc++.h"
-#define ll long long
-using namespace std;
+#include"cstdio"
+void sieve(bool a[],int n){
+  for (int i=0;i<=n;i++){a[i]=true;}
+  a[0]=a[1]=false;
+  for (int i=2;i*i<=n;i++){
+    if (a[i]){
+      for (int j=i*i;j<=n;j+=i){a[j]=false;}
+    }
+  }
+}
 int main(){
-	int n,x;bool found=false;;cin>>n>>x;ll a[n];
-	for(int i=0;i<n;i++){cin>>a[i];}
-	for(int i=0;i<n;i++){if(i==x){found=true;}}
-	if(found==true){cout<<"YES";}else{cout<<"NO";}
+	int n,d=0;scanf("%i",&n);bool p[n];sieve(p,n);
+	for(int i=0;i<=n;i++){if(p[i]){d++;}}printf("%d",d);
 }
--- why-am-i-learning-cpp-now/tempFile.cpp
+++ why-am-i-learning-cpp-now/tempFile.cpp
@@ -1,18 +1,16 @@
 #include"iostream"
+#include"string"
 using namespace std;
-void eratosthenes_sieve(bool arr[],int n){
-  for (int i=0;i<=n;i++){arr[i]=true;}
-  arr[0]=arr[1]=false;
-  for (int i=2;i*i<=n;i++){
-    if (arr[i]){
-      for (int j=i*i;j<=n;j+=i){arr[j]=false;}
-    }
-  }
+bool xaudoixung(string s){
+	int n=s.length();bool dx=true;
+	for(int i=0;i<n;i++){if(s[i]!=s[n-i-1]){dx=false;}}
+	return dx;
 }
 int main(){
-    int n;cin>>n;bool cock[n];
-    eratosthenes_sieve(cock,n);
-    for(int i=2;i<=n;i++){
-        if(cock[i]==true){cout<<cock[i]<<" ";}
-    }
+	string s;int q;cin>>s>>q;
+	for(int o=1;o<=q;o++){
+		int i,j;cin>>i>>j;
+		string tmp=s.substr(i-1,j-i+1);
+		if(xaudoixung(tmp)==true){cout<<"YES\n";}else{cout<<"NO\n";}
+	}
 }
