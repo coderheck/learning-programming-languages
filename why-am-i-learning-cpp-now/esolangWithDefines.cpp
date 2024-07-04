f05a4d2a27b08cd192723f2b2487e9264d70abc9
coderheck <loxzyioxzybusiness@hotmail.com>
2024-07-04 14:13:01 +0700

    oh they also work in headers

--- why-am-i-learning-cpp-now/esolangWithDefines.cpp
+++ why-am-i-learning-cpp-now/esolangWithDefines.cpp
@@ -1,47 +1,5 @@
-#define chief main
-#define nocap true
-#define cap false
-#define lawg bool
-#define yeet return
-#define sussin if
-#define shit else
-#define bussin for
-#define tweakin while
-#define gotta std
-#define felloff endl
-#define spit cout
-#define slurp cin
-#define gonna {
-#define tho }
-#define fr <<
-#define deadass >>
-#define rn ;
-#define mf int
-#define nah 0
-#define be =
-#define finna ==
-#define smallASF <=
-#define bigASF >=
-#define small <
-#define big >
-#define fuck %
-#define extra +
-#define waste -
-#define merge *
-#define cut /
-#define nope !
-#define stepup ++
-#define bitches "iostream"
+#include"esolang.h"
 #include bitches
-
-lawg prime(mf n) gonna
-    lawg p be nocap rn
-    sussin(n smallASF 1) gonna p be cap rn tho
-    bussin(mf i be 2 rn i smallASF n cut 2 rn i stepup) gonna
-        sussin(n fuck i finna 0) gonna p be cap rn tho
-    tho
-    yeet p rn
-tho
 
 mf chief() gonna
     mf n be nah rn
--- why-am-i-learning-cpp-now/temp.cpp
+++ why-am-i-learning-cpp-now/temp.cpp
@@ -1,30 +1,6 @@
-#include"iostream"
-#include"vector"
-#include"utility"
-#define ll long long
-using namespace std;
-vector<pair<ll,ll>>factorize(int n){
-  vector<pair<ll,ll>>factors;
-  factors[0].first=2; factors[0].second=0;
-  ll iter=1;
-  while(n%2==0){factors[0].second++;n/=2;}
-  for(ll i=3;i*i<=n;i+=2){
-    factors[iter].first=i;
-    factors[iter].second=0;
-    while(n%i==0){
-      factors[iter].second++;
-      n/=2;
-    }
-    iter++;
-  }
-  if(n>2){factors[iter].first=n;factors[iter].second=1;}
-  return factors;
-}
+#include"stdio.h"
+#include"cmath"
 int main(){
-  ll num=315;
-  vector<pair<ll,ll>>facts=factorize(num);
-  cout<<num<<": ";
-  for(pair<ll,ll>i: facts){
-    if(i.second>0){cout<<i.first<<" - "<<i.second<<"\n";}
-  }
+  long double pi=2*std::acos(0.0);
+  printf("%f",pi);
 }
