7986b7f81e797c1204e5bc267b56f237df4fa041
coderheck <loxzyioxzybusiness@hotmail.com>
2024-07-23 14:29:41 +0700

    sometimes i want to fucking kill myself

--- why-am-i-learning-cpp-now/grade 9/thptchuyen/2203d_palin-incomplete.cpp
+++ why-am-i-learning-cpp-now/grade 9/thptchuyen/2203d_palin-incomplete.cpp
@@ -1,19 +1,27 @@
 #include"iostream"
 #include"string"
 using namespace std;
+string palin(string s,int i,int j){
+  int i2=i+1,j2=j-1;
+  if(j-i+1==1){return"YES\n";}
+  while(i<j){
+    if(s[i]!=s[j]){return"NO\n";break;}
+    i+=2;j-=2;
+  }
+  while(i2<j2){
+    if(s[i2]!=s[j2]){return"NO\n";break;}
+    i2+=2;j2-=2;
+  }
+  return"YES\n";
+}
 int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(0);
   string s;int q;cin>>s>>q;
   for(int n=1;n<=q;n++){
     int i,j;cin>>i>>j;
-    string res("YES\n");
     i--;j--;
-    while(i<j){
-      if(s[i]!=s[j]){res="NO\n";break;}
-      i++;j--;
-    }
-    cout<<res;
+    cout<<palin(s,i,j);
   }
 }
 // abccbabccdcdcd 5 1 6 5 7 8 12 4 9 10 14
