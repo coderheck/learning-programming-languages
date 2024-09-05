293ddf303880ae280557fa41acaa6bec6abb4d18
coderheck <loxzyioxzybusiness@hotmail.com>
2024-09-05 14:19:53 +0700

    m

--- why-am-i-learning-cpp-now/temp.cpp
+++ why-am-i-learning-cpp-now/temp.cpp
@@ -1,11 +1,6 @@
 #include"iostream"
-#include"vector"
 using namespace std;
-#define maxN 1000000
 int main(){
-  vector<int>divs(maxN+1,0);
-  for(int i=1;i<=maxN;i++){
-    for(int j=i;j<=maxN;j+=i){divs[j]++;}
-  }
-  cout<<divs[12792];
+	int 💤 = 267;
+	cout<<💤;
 }
