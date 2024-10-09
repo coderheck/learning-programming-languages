67ed94b7e05de32060538a2e28c734b417972f8d
coderheck <loxzyioxzybusiness@hotmail.com>
2024-10-09 14:15:30 +0700

    nice

--- why-am-i-learning-cpp-now/grade 9/ThayKien/oct 6th 2024/horse.cpp
+++ why-am-i-learning-cpp-now/grade 9/ThayKien/oct 6th 2024/horse.cpp
@@ -3,17 +3,18 @@
 using namespace std;
 #define ll long long
 #define maxN 100000
-int n,it=0,c=0;ll a[maxN],b[maxN];
+int n,c=0;ll a[maxN],b[maxN];
 int main(){
   cin>>n;
   for(int i=0;i<n;i++){cin>>a[i];}
   for(int i=0;i<n;i++){cin>>b[i];}
-  sort(a,a+n/2,greater<ll>());sort(b,b+n/2);
-  for(int i=0;i<n;i++){cout<<a[i]<<" ";}
-  cout<<"\n";
-  for(int i=0;i<n;i++){cout<<b[i]<<" ";}
-  while(it<n){
-    if(a[it]>b[it]){c++;it++;}else{it++;}
+  sort(a,a+n);sort(b,b+n);
+  int j=0;
+  for(int i=0;i<n;i++){
+    if(a[i]>b[j]){
+      c++;j++;
+    }
+    if(j==n){break;}
   }
-  cout<<"\n"<<c;
+  cout<<c;
 }
