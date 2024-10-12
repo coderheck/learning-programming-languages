991dcb55cddc1074e88ccbe5c4b12f4e77c1dfdd
coderheck <loxzyioxzybusiness@hotmail.com>
2024-10-12 09:58:15 +0700

    die

--- why-am-i-learning-cpp-now/temp.cpp
+++ why-am-i-learning-cpp-now/temp.cpp
@@ -1,16 +1,48 @@
 #include"iostream"
-#include"vector"
 using namespace std;
 #define ll long long
-ll sumOfDivisors(ll n){
-  ll sum=0;
-  for(ll i=2;i*i<=n;i++){
-    if(n%i==0){
-      if(i==(n/i)){sum+=i;}else{sum+=(i+(n/i));}
-    }
-  }
-	return (sum+1);
+#define maxN 2000000
+bool pr[maxN+1];
+void prsieve() {
+	for(int i=2; i<=maxN; i++) {
+		pr[i]=true;
+	}
+	pr[0]=pr[1]=false;
+	for(int i=2; i*i<=maxN; i++) {
+		if(pr[i]) {
+			for(int j=i*i; j<=maxN; j+=i) {
+				pr[j]=false;
+			}
+		}
+	}
 }
-int main(){
-  long long n;cin>>n;cout<<sumOfDivisors(n);
+bool bacthang(ll a) {
+	if(a<10) {
+		return false;
+	}
+	int r=a%10,l=(a/10)%10;
+	while(a!=0&&a/10!=0) {
+		if(r<=l) {
+			return false;
+		}
+		a/=10;
+		r=a%10,l=(a/10)%10;
+	}
+	return true;
+}
+int n,a,r=0;
+int main() {
+	prsieve();
+	if(fopen("temp.inp","r")) {
+		freopen("temp.inp","r",stdin);
+		//freopen("temp.out","w",stdout);
+	}
+	cin>>n;
+	for(int i=1; i<=n; i++) {
+		cin>>a;
+		if(pr[a]&&bacthang(a)&&a>r) {
+			r=a;
+		}
+	}
+	cout<<r;
 }
