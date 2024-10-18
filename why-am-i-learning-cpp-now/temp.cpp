3ca3614af28ece0be353f33715cef57b9d4b1c58
coderheck <loxzyioxzybusiness@hotmail.com>
2024-10-18 16:43:29 +0700

    hí as wilding out bruh

--- why-am-i-learning-cpp-now/temp.cpp
+++ why-am-i-learning-cpp-now/temp.cpp
@@ -1,16 +1,28 @@
 #include"iostream"
+#include"vector"
 #include"algorithm"
 using namespace std;
 #define ll long long
-ll x,y,ans=0;
+vector<ll>divs;
+ll n,k,d=0;
+void demuoc(){
+  for(ll i=1;i*i<=n;i++){
+    if(n%i==0){
+      d++;divs.push_back(i);
+      ll j=n/i;
+      if(j!=i){d++;divs.push_back(j);}
+    }
+  }
+  if(d>=k){sort(divs.begin(),divs.end());}
+}
 int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
   if(fopen("temp.inp","r")){
     freopen("temp.inp","r",stdin);
     freopen("temp.out","w",stdout);
   }
-  cin>>x>>y;
-  for(ll i=x;i<=y;i++){
-     if(i%3==0){ans+=i;}
-  }
-  cout<<ans<<"\n";
+  cin>>n>>k;
+  demuoc();
+  d<k?cout<<-1:cout<<divs[k-1];
 }
