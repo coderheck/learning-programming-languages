a159f5331fd110c45be8018a894a8d28af6ae4b1
coderheck <loxzyioxzybusiness@hotmail.com>
2024-11-04 22:17:22 +0700

    clean

--- why-am-i-learning-cpp-now/grade 9/ThayKien/july 28th 2024/tudep.cpp
+++ why-am-i-learning-cpp-now/grade 9/ThayKien/july 28th 2024/tudep.cpp
@@ -4,24 +4,15 @@
 using namespace std;
 #define ll long long
 bool isGoodWord(string s){
-  if(s[0]=='u'||s[0]=='e'||s[0]=='o'||s[0]=='a'||s[0]=='i'){
-    ll i=1;
-    while((i+1)<s.size()){
-      if(s[i+1]<'a'||s[i+1]>'z'){return false;}
-      i++;
-    }
-    return true;
-  }else{return false;}
+  return(s[0]=='u'||s[0]=='e'||s[0]=='o'||s[0]=='a'||s[0]=='i')?true:false;
 }
-ll ParseString(string s){
+void ParseString(string s){
   stringstream ss(s);string tmp;ll c=0;
-  while(ss>>tmp){
-    if(isGoodWord(tmp)){c++;}
-  }
-  return c;
+  while(ss>>tmp){c+=isGoodWord(tmp);}
+  cout<<c;
 }
 int main(){
   ll c=0;string s;
   getline(cin,s);getline(cin,s);
-  cout<<ParseString(s);
+  ParseString(s);
 }
