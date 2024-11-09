d4ba045a3532ed7a8f889b66c3013dccea82ffb8
coderheck <loxzyioxzybusiness@hotmail.com>
2024-11-09 16:12:54 +0700

    VAILONCONCHON

--- why-am-i-learning-cpp-now/grade 9/ThayKien/nov 7th 2024/example2.cpp
+++ why-am-i-learning-cpp-now/grade 9/ThayKien/nov 7th 2024/example2.cpp
@@ -1,14 +1,16 @@
 #include"iostream"
 using namespace std;
-#define maxn 1000000
-int n,res=0,s,a[maxn+5],cnt[maxn+5],sum[maxn+5];
+#define maxN 1000000
+int n,s,cnt[maxN+5],a[maxN+5],sum[maxN+5],res=0;
 int main(){
   cin>>n>>s;
   cnt[0]=1;
   for(int i=0;i<n;i++){
     cin>>a[i];
     sum[i]=sum[i-1]+a[i];
-    if(sum[i]>=s){res+=cnt[sum[i]-s];}
+    if(sum[i]>=s){
+      res+=cnt[sum[i]-s];
+    }
     cnt[sum[i]]++;
   }
   cout<<res;
