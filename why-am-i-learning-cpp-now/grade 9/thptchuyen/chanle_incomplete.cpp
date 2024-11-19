30bbef5a3ac8bf6d1b68a90b075895913e3d2fbb
coderheck <loxzyioxzybusiness@hotmail.com>
2024-11-19 15:13:05 +0700

    WHAT THE FUCK ARE THOSE EXERCISES

--- why-am-i-learning-cpp-now/grade 9/thptchuyen/chanle_incomplete.cpp
+++ why-am-i-learning-cpp-now/grade 9/thptchuyen/chanle_incomplete.cpp
@@ -2,23 +2,9 @@
 #include"algorithm"
 using namespace std;
 #define ll long long
+ll l,r,ta,tb,even,odd;
 int main(){
-  ll a,b,ta,tb;cin>>a>>b;
-  if(a%2==0 && b%2==0){
-    ta=(a+b)*((b-a)/2+1)/2;
-    tb=(a+b)*((b-a)/2+1)/2;
-  }
-  if(a%2==1 && b%2==0){
-    ta=(a+b+1)*((b-a+1)/2+1)/2;
-    tb=(a+b-1)*((b-a-1)/2+1)/2;
-  }
-  if(a%2==0 && b%2==1){
-    ta=(a+b-1)*((b-a+1)/2+1)/2;
-    tb=(a+b+1)*((b-a-1)/2+1)/2;
-  }
-  if(a%2==1 && b%2==1){
-    ta=(a+b)*((b-a)/2+1)/2;
-    tb=(a+b)*((b-a)/2+1)/2;
-  }
-  cout<<max(ta,tb)-min(ta,tb);
+  cin>>l>>r;
+  even=(r+1-l)/2;odd=r+1-l-even;
+  cout<<even<<" "<<odd;
 }
--- why-am-i-learning-cpp-now/grade 9/thptchuyen/sumx.cpp
+++
@@ -1,7 +0,0 @@
-#include"iostream"
-using namespace std;
-int n,x,a[100005],cnt[100005];
-int main(){
-cout<<"LONG YEU NHI\n";
-cout<<"DUCA";
-}
