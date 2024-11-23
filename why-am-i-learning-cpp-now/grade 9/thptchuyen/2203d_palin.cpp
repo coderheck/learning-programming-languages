2ac5be2c1201bcdf6445024ab795d21bd9e71cdc
coderheck <loxzyioxzybusiness@hotmail.com>
2024-11-23 22:42:41 +0700

    for public

--- why-am-i-learning-cpp-now/grade 9/thptchuyen/2203d_palin.cpp
+++ why-am-i-learning-cpp-now/grade 9/thptchuyen/2203d_palin.cpp
@@ -1,24 +1,18 @@
 #include"stdio.h"
-#include"string"
-using namespace std;
+#include"string.h"
 #define ull unsigned long long
-#define MOD 1000000007
-#define maxN 100000
-#define p 101
-string s;int q,n;char str[maxN+1];
+const ull MOD=1000000007;
+const int maxN=100000;
+const int p=101;
+struct Q{int L,R;}a[maxN+1];
+int q,n;char s[maxN+1];
 ull pref[maxN+1],suff[maxN+1],pows[maxN+1];
-bool ispl(int l,int r){
-  while(r>l){
-    if(s[l++]!=s[r--]){return false;}
-  }
-  return true;
-}
 ull fastpowmod(ull a,ull b){
-  ull res=1; a%=MOD;
-  if(a==0)return 0;
+  ull res=1;a%=MOD;
+  if(a==0){return 0;}
   while(b>0){
     if(b&1){res=(res*a)%MOD;}
-    b>>=1; a=(a*a)%MOD;
+    b>>=1;a=(a*a)%MOD;
   }
   return res;
 }
@@ -26,17 +20,17 @@ ull modMulInv(ull n){return fastpowmod(n,MOD-2);}
 void prefixHash(){
   pref[0] = 0;
   pref[1] = s[0];
-  for (int i = 2; i <= n; i++)
+  for(int i=2;i<=n;i++)
     pref[i] = (
-      pref[i - 1] % MOD + (s[i - 1] % MOD * pows[i - 1] % MOD) % MOD
+      pref[i-1]%MOD + (s[i-1]%MOD * pows[i-1]%MOD) % MOD
     ) % MOD;
 }
 void suffixHash(){
   suff[0] = 0;
   suff[1] = s[n - 1];
-  for (int i = n - 2, j = 2; i >= 0 && j <= n; i--, j++)
+  for (int i=n-2,j=2;i>=0&&j<=n;i--,j++)
     suff[j] = (
-      suff[j-1]%MOD + (s[i]%MOD * pows[j-1]%MOD) % MOD
+      suff[j-1]%MOD + (s[i]%MOD * pows[j-1]%MOD)%MOD
     ) % MOD;
 }
 void computepows(){
@@ -45,9 +39,8 @@ void computepows(){
     pows[i]=(pows[i-1]%MOD*p%MOD)%MOD;
 }
 void parse(){
-  for(int i=1;i<=q;i++){
-    int l,r;scanf("%d%d",&l,&r);
-    l--;r--;
+  for(int i=0;i<q;i++){
+    int l=a[i].L-1,r=a[i].R-1;
     ull forwardHash = (
       (pref[r+1]-pref[l]+MOD) % MOD * modMulInv(pows[l]) % MOD
     ) % MOD;
@@ -62,9 +55,12 @@ void parse(){
   }
 }
 int main(){
-  scanf("%[^\n]s",str);
+  scanf("%[^\n]s",s);
   scanf("%d",&q);
-  s=str;n=s.size();
+  for(int i=0;i<q;i++){
+    scanf("%d%d",&a[i].L,&a[i].R);
+  }
+  n=strlen(s);
   computepows();prefixHash();suffixHash();
   parse();
 }
@@ -79,4 +75,4 @@ NO
 YES
 */
 
-// 
+// I LOVE YOU SO MUCH STRINGS AUHUAGAHUGAHHU IMBATUKAMMMMM AHHHHHHHH
