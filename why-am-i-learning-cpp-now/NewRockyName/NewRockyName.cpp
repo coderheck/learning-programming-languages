8d10c884c66047352c3c65b7f4941e4321423138
coderheck <loxzyioxzybusiness@hotmail.com>
2024-12-08 18:03:39 +0700

    newrockyname new ver

--- why-am-i-learning-cpp-now/NewRockyName/NewRockyName.cpp
+++ why-am-i-learning-cpp-now/NewRockyName/NewRockyName.cpp
@@ -5,19 +5,29 @@
 using namespace std;
 int main(int argCount, char *argValue[]){
 	string toAdd="";
-	fstream ffile("C:\\Users\\datmobile\\Documents\\rocky names.txt",ios::app);
+	cout<<"arg count: "<<argCount<<"\n";
 	if(argCount==1){
 		cout<<"you didnt specify any arguments, now type (or paste) the new name here: ";
 		getline(cin,toAdd);
+		fstream ffile("C:\\Users\\datmobile\\Documents\\rocky names.txt",ios::app);
 		ffile<<toAdd<<"\n";
 		ffile.close();
 		system("\"C:\\Users\\datmobile\\Documents\\rocky names.txt\"");
+		return 0;
+	}else if(strcmp(argValue[1],"--file")==0){
+		cout<<argValue[1]<<" file";
+		system("\"C:\\Users\\datmobile\\Documents\\rocky names.txt\"");
+		return 0;
 	}else{
+		cout<<argValue[0]<<" add";
 		for(int i=1;i<argCount;i++){
 			if(i+1!=argCount){toAdd=toAdd+argValue[i]+" ";}else{toAdd=toAdd+argValue[i];}
 		}
+		fstream ffile("C:\\Users\\datmobile\\Documents\\rocky names.txt",ios::app);
 		ffile<<toAdd<<"\n";
 		ffile.close();
 		system("\"C:\\Users\\datmobile\\Documents\\rocky names.txt\"");
+		return 0;
 	}
+	return 0;
 }
