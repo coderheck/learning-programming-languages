3d866076b03700a1c68ee0a9f48b482617729075
coderheck <loxzyioxzybusiness@hotmail.com>
2024-12-08 11:28:53 +0700

    morning

--- why-am-i-learning-cpp-now/grade 9/TrenTruong/dec 7th 2024/longlong9a_07-12-2024_toi/chosebook.cpp
+++ why-am-i-learning-cpp-now/grade 9/TrenTruong/dec 7th 2024/longlong9a_07-12-2024_toi/chosebook.cpp
@@ -1,10 +1,16 @@
 #include"iostream"
+#include"algorithm"
 using namespace std;
 #define ll long long
 const int maxN=10000000;
-int n,m;ll a[maxN+5];
+ll min(ll a,ll b){return(a<b)?a:b;}
+int n,m;ll a[maxN+5],minDiff=LLONG_MAX;
 int main(){
   cin>>n>>m;
   for(int i=0;i<n;i++){cin>>a[i];}
-  
+  sort(a,a+n);
+  for(int i=0;i<=n-m;i++){
+    minDiff=min(minDiff,a[i+m-1]-a[i]);
+  }
+  cout<<minDiff;
 }
