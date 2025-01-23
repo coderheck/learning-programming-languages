984c3e9ac7a672222c2642be42ed68a86ef6dafc
coderheck <loxzyioxzybusiness@hotmail.com>
2025-01-23 16:29:56 +0700

    minor edits

--- why-am-i-learning-cpp-now/grade 9/thptchuyen/clis.cpp
+++ why-am-i-learning-cpp-now/grade 9/thptchuyen/clis.cpp
@@ -1,14 +1,13 @@
 #include"iostream"
 #include"string"
 using namespace std;
-#define ll long long
-string s;ll res=1,cur=1,n;
+string s;int res=1,cur=1,n;
 int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
   cin>>s;n=s.size();
-  for(ll i=1;i<n;i++){
-    if(s[i]>=s[i-1]){cur++;}else{if(res<cur){res=cur;};cur=1;}
+  for(int i=1;i<n;i++){
+    if((int)s[i]>=(int)s[i-1]){cur++;}else{if(res<cur){res=cur;};cur=1;}
   }
   if(res<cur){res=cur;}
   cout<<res;
