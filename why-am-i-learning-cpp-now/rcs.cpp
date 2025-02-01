261444ff3ebb14c378168f25f8b14bc3a9de3977
coderheck <loxzyioxzybusiness@hotmail.com>
2025-02-02 00:02:01 +0700

    wtf

--- why-am-i-learning-cpp-now/rcs.cpp
+++ why-am-i-learning-cpp-now/rcs.cpp
@@ -1,43 +1,44 @@
 #include<iostream>
 #include<windows.h>
 #include<filesystem>
-using namespace std;
-namespace fs=filesystem;
+namespace fs=std::filesystem;
 int main(){
   const char *subkey="roblox-player\\shell\\open\\command";
   const char *valname="version";
   char buffer[256];
   DWORD buffersize=sizeof(buffer);
   if(ERROR_SUCCESS==RegGetValueA(HKEY_CLASSES_ROOT,subkey,valname,RRF_RT_REG_SZ,nullptr,buffer,&buffersize)){
-    cout<<"version: "<<buffer<<"\n";
+    std::cout<<"version: "<<buffer<<"\n";
   }else{
-    cerr<<"fucking dies";
+    std::cerr<<"fucking dies";
     return 1;
   }
   fs::path robloxPath="C:\\Program Files (x86)\\Roblox\\Versions";
   robloxPath/=buffer; //append version
   robloxPath/="ClientSettings"; //append "ClientSettings"
-  error_code err;
+  std::error_code err;
   if(fs::exists(robloxPath)){
-    cout<<"found current ClientSettings, attempting to delete...\n";
+    std::cout<<"found current ClientSettings, attempting to delete...\n";
     fs::remove_all(robloxPath,err);
     if(err){
-      cerr<<"sum shit happened while deleting current ClientSettings: "<<err.message();
+      std::cerr<<"sum shit happened while deleting current ClientSettings: "<<err.message();
       return 1;
     }
-    cout<<"delete success\n";
+    std::cout<<"delete success\n";
   }
-  fs::path srcPath="ClientSettings/ClientAppSettings.json"; //file to copy
+  wchar_t settingsPath[1024] = {0};
+  GetModuleFileNameW(nullptr,settingsPath,1024);
+  fs::path srcPath = fs::path(settingsPath).parent_path()/"ClientSettings/ClientAppSettings.json"; //file to copy
   try{
     fs::create_directories(robloxPath);
   }catch(const fs::filesystem_error &e){
-    cerr<<"sum shit happened while making new ClientSettings folder: "<<e.what();
+    std::cerr<<"sum shit happened while making new ClientSettings folder: "<<e.what();
   }
   try{
     fs::copy_file(srcPath,robloxPath/srcPath.filename(),fs::copy_options::overwrite_existing);
   }catch(const fs::filesystem_error &e){
-    cerr<<"sum shit happened while copying new ClientSettings: "<<e.what();
+    std::cerr<<"sum shit happened while copying new ClientSettings: "<<e.what();
     return 1;
   }
-  cout<<"copy ClientSettings success";
+  std::cout<<"copy ClientSettings success";
 }
--- why-am-i-learning-cpp-now/temp.cpp
+++ why-am-i-learning-cpp-now/temp.cpp
@@ -1,17 +1,9 @@
 #include"iostream"
-#include"string"
+#include"windows.h"
+#include"filesystem"
 using namespace std;
-string s,deco;
 int main(){
-  freopen("temp.inp","r",stdin);
-  cin>>s;
-  for(int i=0;i<s.size();i++){if(s[i]=='#'){s.erase(i,1);}}
-  for(int i=0;i<s.size();i++){
-    if(s[i]=='&'){
-      int c=0;i++;
-      while(s[i]!=';'){c=c*10+s[i++]-'0';}
-      deco.push_back((char)c);
-    }
-  }
-  cout<<deco;
+  wchar_t path[2048]={0};
+  GetModuleFileNameW(nullptr,path,2048);
+  cout<<std::filesystem::path(path).parent_path();
 }
