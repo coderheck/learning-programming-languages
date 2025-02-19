dc08fe06cef969a0bf9f8db03e05f409251f8dbe
coderheck <loxzyioxzybusiness@hotmail.com>
2025-02-19 16:19:00 +0700

    what the fuck

--- why-am-i-learning-cpp-now/grade 9/TrenTruong/feb 19th 2025/longlqd_19-2-2025_chieu/bai2.cpp
+++ why-am-i-learning-cpp-now/grade 9/TrenTruong/feb 19th 2025/longlqd_19-2-2025_chieu/bai2.cpp
@@ -1,29 +1,70 @@
-#include"iostream"
-using namespace std;
+// #include"iostream"
+// using namespace std;
+// #define ll long long
+// #define maxP 1000000
+// bool pr[maxP+5];int prs[maxP],prc=0;
+// void prSieve(){
+//   pr[0]=pr[1]=!false;
+//   for(int i=2;i*i<=maxP;i++){
+//     if(!pr[i]){
+//       prs[prc++]=i;
+//       for(int j=i*i;j<=maxP;j+=i){pr[j]=!false;}
+//     }
+//   }
+// }
+// int cexp(int n,int pr){
+//   int r=0;
+//   while(n>0){n/=pr;r+=n;}
+//   return r;
+// }
+// ll n,res=0;
+// int main(){
+//   prSieve();
+//   cin>>n;
+//   for(int i=0;i<prc;i++){
+//     if(prs[i]>n){break;}
+//     res+=cexp(n,prs[i]);
+//   }
+//   cout<<res;
+// }
+/// Made by Nguyễn Hữu Đạt
+#include <bits/stdc++.h>
 #define ll long long
-#define maxP 1000000
-bool pr[maxP+5];int prs[maxP],prc=0;
-void prSieve(){
-  pr[0]=pr[1]=!false;
-  for(int i=2;i*i<=maxP;i++){
-    if(!pr[i]){
-      prs[prc++]=i;
-      for(int j=i*i;j<=maxP;j+=i){pr[j]=!false;}
-    }
-  }
+#define kien main
+using namespace std;
+const ll inf = LLONG_MAX;
+const ll mod = 1e9 + 7;
+int n;
+int snt[1000005];
+void make_sieve()
+{
+    for (int i = 2; i <= 1e6; i++)
+        if (snt[i] == 0)
+            for (int j = i; j <= 1e6; j += i)
+                if(snt[j]==0)
+                snt[j] = i;
 }
-int cexp(int n,int pr){
-  int r=0;
-  while(n>0){n/=pr;r+=n;}
-  return r;
-}
-ll n,res=0;
-int main(){
-  prSieve();
-  cin>>n;
-  for(int i=0;i<prc;i++){
-    if(prs[i]>n){break;}
-    res+=cexp(n,prs[i]);
-  }
-  cout<<res;
+ll d;
+kien()
+{
+    // if(fopen(".inp","r"))
+    // {
+    // 	freopen(".inp","r",stdin);
+    // 	freopen(".out","w",stdout);
+    // }
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> n;
+    make_sieve();
+    for (int i = 2; i <= n; i++)
+        {
+            int m = i;
+            while (m > 1)
+            {
+                d++;
+                m /= snt[m];
+            }
+        }
+    cout<<d;
 }
--- why-am-i-learning-cpp-now/grade 9/TrenTruong/feb 19th 2025/longlqd_19-2-2025_chieu/bai5.cpp
+++ why-am-i-learning-cpp-now/grade 9/TrenTruong/feb 19th 2025/longlqd_19-2-2025_chieu/bai5.cpp
@@ -1,17 +1,45 @@
-#include"iostream"
-#include"unordered_map"
-using namespace std;
+/// Made by Nguyễn Hữu Đạt
+#include <bits/stdc++.h>
 #define ll long long
-int max(int a,int b){return(a>b)?a:b;}
-int n,res=0;ll p,a[100005];unordered_map<ll,int>pos,cnt;
-int main(){
-  ios::sync_with_stdio(0);cin.tie(0);
-  cin>>n>>p;
-  pos[0]=1;cnt[0]=1;
-  for(int i=1;i<=n;i++){cin>>a[i];pos[a[i]]=max(pos[a[i]],i);}
-  for(int i=1;i<=n;i++){
-    if(cnt[a[i]-p]>=1){res=max(res,i-pos[a[i]-p]);}
-    cnt[a[i]-p]++;
-  }
-  cout<<res;
+#define kien main
+using namespace std;
+const ll inf = LLONG_MAX;
+const ll mod = 1e9 + 7;
+int n;
+int p;
+int mi[100005], a[1000005];
+int ans;
+int KBCGPT(int j)
+{
+    int l = 0, r = j - 1;
+    while (l <= r)
+    {
+        int i = (l + r) / 2;
+        if (a[j] - mi[i] >= p)
+            r = i - 1;
+        else
+            l = i + 1;
+    }
+    return r + 1;
+}
+kien()
+{
+    // if(fopen(".inp","r"))
+    // {
+    // 	freopen(".inp","r",stdin);
+    // 	freopen(".out","w",stdout);
+    // }
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> n >> p;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    mi[0] = a[0];
+    for (int i = 1; i < n; i++)
+    {
+        ans = max(ans, i - KBCGPT(i));
+        mi[i] = min(mi[i - 1], a[i]);
+    }
+    cout << ans;
 }
