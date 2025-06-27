4068a47b6138c679649a464e91de48db50ac21f2
coderheck <loxzyioxzybusiness@hotmail.com>
2025-06-27 08:48:57 +0700

    directory name change

---
+++ why-am-i-learning-cpp-now/itk8intro.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+#include<windows.h>
+
+std::string rep = "", goal = "ITK8 - GNU Compiler Collection";
+
+int main(){
+    while (rep != goal)
+    {
+        
+    }
+}
