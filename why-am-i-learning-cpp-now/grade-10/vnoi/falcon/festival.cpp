deb26fdf3b7195a314712a006f00661b0ba32628
coderheck <loxzyioxzybusiness@hotmail.com>
2026-01-14 16:24:42 +0700

    schooly pushes (2026-01-14)

--- why-am-i-learning-cpp-now/grade-10/vnoi/falcon/festival.cpp
+++ why-am-i-learning-cpp-now/grade-10/vnoi/falcon/festival.cpp
@@ -12,28 +12,28 @@ ll n,m,s,x,a[maxN][maxN],sum[maxN][maxN],zz[maxN][maxN];
 int main(){
     if(fopen(tname".inp","r")){
         freopen(tname".inp","r",stdin);
-        // freopen(tname".out","w",stdout);
+        freopen(tname".out","w",stdout);
     }
     cin>>n>>m>>s>>x;
     for(ll i=1;i<=n;i++){
         for(ll j=1;j<=m;j++){
             cin>>a[i][j];
             sum[i][j]=sum[i-1][j]+sum[i][j-1]-sum[i-1][j-1]+a[i][j];
-            zz[i][j]=zz[i-1][j]+zz[i][j-1]-zz[i-1][j-1]+a[i][j]==0;
+            zz[i][j]=zz[i-1][j]+zz[i][j-1]-zz[i-1][j-1]+(a[i][j]==0);
         }
     }
     // cout<<sum[u][v]-sum[i-1][j]-sum[i][j-1]+sum[i-1][j-1];
-    ll res=0;
+    ll res=-1;
     for(ll i=1;i<=n;i++){
         for(ll j=1;j<=m;j++){ // (i,j) góc trên bên trái
             for(ll u=i;u<=n;u++){
                 for(ll v=j;v<=m;v++){ // (u,v) góc dưới bên phải
-                    if(zz[u][v]-zz[u-i][v]-zz[u][v-j]+zz[u-i][v-j]<=x && (u-i+1)*(v-j+1)<=s){
-                        res=max(res,sum[u][v]-sum[u-i][v]-sum[u][v-i]+sum[u-i][v-j]);
+                    if(zz[u][v]-zz[i-1][v]-zz[u][j-1]+zz[i-1][j-1]<=x && (u-i+1)*(v-j+1)<=s){
+                        res=max(res,sum[u][v]-sum[i-1][v]-sum[u][j-1]+sum[i-1][j-1]);
                     }
                 }
             }
         }
     }
-    cout<<(!res?-1:res);
+    cout<<res;cerr<<res;
 }
--- why-am-i-learning-cpp-now/grade-10/vnoi/falcon/flower123.cpp
+++ why-am-i-learning-cpp-now/grade-10/vnoi/falcon/flower123.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
+#include <queue>
 #include <vector>
 #include <cmath>
 using namespace std;
-#define tname "flower"
+#define tname "flower123"
 #define ll long long
 #define debug(x) cout<<(x)<<" ";
 #define max(a,b) ((a)>(b)?(a):(b))
 #define min(a,b) ((a)<(b)?(a):(b))
 const ll inff=1000000000007;
-ll w,h,n,k;
+ll w,h,n,k,dx[4]={1,0,-1,0},dy[4]={0,1,0,-1};
 struct pll{ll x,y;};
 vector<pll>fl;
+ll solve(const ll &x,const ll &y){
+	ll sum=0;
+	for(const pll &p:fl){
+		ll d=abs(p.x-x)+abs(p.y-y);
+		if(d<k){sum+=(k-d);}
+	}
+	return sum;
+}
 int main(){
     if(fopen(tname".inp","r")){
         freopen(tname".inp","r",stdin);
@@ -18,7 +27,6 @@ int main(){
     }
     cin.tie(0)->sync_with_stdio(0);
     cin>>w>>h>>n>>k;
-    vector<vector<ll>>dp(w+5,vector<ll>(h+5,0));
     if(n==1){
         ll x,y,res=inff;cin>>x>>y;
         for(ll i=1;i<=w;i++){
@@ -34,28 +42,31 @@ int main(){
         cout<<res;
         return 0;
     }
-    for(ll i=1;i<=n;i++){
-        ll x,y;cin>>x>>y;
-        fl.push_back({x,y});
-        dp[x][y]=k;
-    }
-    for(ll i=1;i<=w;i++){
-        for(ll j=1;j<=h;j++){
-            ll mindist=inff;
-            for(const pll &p:fl){
-                mindist=min(mindist,abs(p.x-i)+abs(p.y-j));
-            }
-            if(k-mindist<0){cout<<"0";return 0;}
-            dp[i][j]=k-mindist;
-        }
-    }
-    ll res=inff;
-    for(ll i=1;i<=w;i++){
-        for(ll j=1;j<=h;j++){
-            // cout<<dp[i][j]<<" ";
-            res=min(res,dp[i][j]);
-        }
-        // cout<<"\n";
-    }
-    cout<<res;
+	vector<vector<ll>>g(w+5,vector<ll>(h+5,-1));
+	queue<pll>q;
+	ll res=inff;
+	for(ll i=1;i<=n;i++){
+		ll x,y;cin>>x>>y;
+		// fl.push_back({x,y});
+		q.push({x,y}),g[x][y]=0;
+	}
+	while(q.size()){
+		ll x=q.front().x,y=q.front().y,cdist=g[x][y];q.pop();
+		if(cdist>=k){continue;}
+		for(ll i=0;i<4;i++){
+			ll u=x+dx[i],v=y+dy[i];
+			if(u>=1&&u<=w&&v>=1&&v<=h&&g[u][v]==-1){
+				g[u][v]=cdist+1,q.push({u,v});
+			}
+		}
+	}
+	for(ll i=1;i<=w;i++){
+		for(ll j=1;j<=h;j++){
+			ll fire=k-g[i][j];
+			if(fire<=0){cout<<"0";cerr<<"0";return 0;}
+			if(fire<res){res=fire;}
+		}
+	}
+	cout<<res;
+	cerr<<res;
 }
