2bb64d71960cde4eb094e919ab5b3190cf0b81d0
coderheck <loxzyioxzybusiness@hotmail.com>
2026-07-09 11:40:24 +0700

    homely pushes (2026-07-09_2)

--- why-am-i-learning-cpp-now/grade-11/vnoi/interactive_guessnumber.cpp
+++ why-am-i-learning-cpp-now/grade-11/vnoi/interactive_guessnumber.cpp
@@ -1,7 +1,6 @@
 // #include <ext/pb_ds/assoc_container.hpp> // for policy hash table
 #include <iostream>
 #include <string>
-#include <algorithm>
 using namespace std;
 #define tname "interactive_guessnumber"
 #define umap __gnu_pbds::gp_hash_table
@@ -14,13 +13,24 @@ using namespace std;
 #define oout(x) \
     cout<<x<<" ";\
 	cerr<<x<<" ";
-const ll maxN=500005;
-ll l=1,r=
+ll l=1,r=2000000000,m;
 int main(){
 	if(fopen(tname".inp","r")){
 		freopen(tname".inp","r",stdin);
 		freopen(tname".out","w",stdout);
 	}
-	// cin.tie(0)->sync_with_stdio(0);
-	while(
+	cin.tie(0)->sync_with_stdio(0);
+	string res;
+	while(true){
+		m=(l+r)>>1;
+		cout<<m<<endl;
+		cin>>res;
+		if(res=="BIGGER"){
+			l=m+1;
+		}else if(res=="SMALLER"){
+			r=m-1;
+		}else{
+			return 0;
+		}
+	}
 }
